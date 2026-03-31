d4de5e98da36c8f256728f9bbe7a689154280254
zhuxa <zhuxueao123@sina.com>
2026-03-31 11:13:39 +0800

    初始化

--- CoreCms.Net.Web.Admin/wwwroot/lib/layuiAdmin/config.js
+++ CoreCms.Net.Web.Admin/wwwroot/lib/layuiAdmin/config.js
@@ -11,8 +11,8 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function (exports) {
         , engine: '.html' //视图文件后缀名
         , pageTabs: false //是否开启页面选项卡功能。单页版不推荐开启
 
-        , name: '核心商城系统'
-        , version: 'CoreShop v1.5.5'
+        , name: 'Qukamao'
+        , version: 'qukamao v1.5.5'
         , tableName: 'CoreCms' //本地存储表名
         , MOD_NAME: 'admin' //模块事件名
 
